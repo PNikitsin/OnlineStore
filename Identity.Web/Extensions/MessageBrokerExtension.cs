ad520dcc4b73ee8182bcf22e222287c75d7340c7
PNikitsin <pnikitinbox@gmail.com>
2023-09-12 03:06:50 +0300

    Added fixes.

--- Identity.Web/Extensions/MessageBrokerExtension.cs
+++ Identity.Web/Extensions/MessageBrokerExtension.cs
@@ -10,6 +10,8 @@ namespace Identity.Web.Extensions
             {
                 config.UsingRabbitMq();
             });
+
+            services.AddMassTransitHostedService();
         }
     }
 }
---
+++ Ordering.Web/Extensions/GrpcServerExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
+
+namespace Ordering.Web.Extensions
+{
+    public static class GrpcServerExtension 
+    {
+        public static WebApplicationBuilder AddGrpcService(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddGrpc();
+
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Any, 80, options =>
+                {
+                    options.Protocols = HttpProtocols.Http1;
+                });
+                options.Listen(IPAddress.Any, 5005, options =>
+                {
+                    options.Protocols = HttpProtocols.Http2;
+
+                });
+            });
+
+            return builder;
+        }
+    }
+}
