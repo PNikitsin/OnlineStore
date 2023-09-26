5a643a6887a3ff7b64c5feea2121ec7b93e64f6d
PNikitsin <pnikitinbox@gmail.com>
2023-09-26 22:59:23 +0300

    Fix.

--- Catalog.Domain/Interfaces/IGenericRepository.cs
+++ Catalog.Domain/Interfaces/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace Catalog.Domain.Interfaces
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
-        Task<T> GetAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
+        Task<T?> GetAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
         Task AddAsync(T entity, CancellationToken cancellationToken = default);
         void Update(T entity);
         void Remove(T entity);
